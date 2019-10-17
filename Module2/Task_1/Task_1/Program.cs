d1d74fcab09d9ab830e3b0f414a0f98fa4523b9d
AdskiGonchi <vpatrin95@mail.ru>
2019-10-17 21:04:55 +0300

    Module 2 task 1 was completed.

---
+++ Module2/Task_1/Task_1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double numbOfCompanies;
+            double tax;
+            double income = 500;
+            double totalTax;
+
+            Console.Write("Введите количество компаний: ");
+            numbOfCompanies = double.Parse(Console.ReadLine());
+            Console.Write("Введите налог в процентах: ");
+            tax = double.Parse(Console.ReadLine());
+
+            totalTax = numbOfCompanies * tax * income / 100;
+            Console.WriteLine("Государство получит суммарный налог: {0} ",totalTax);
+        }
+    }
+}
