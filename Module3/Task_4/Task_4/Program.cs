960cfff99e68cfbf557135179ede78bc35ff67ad
AdskiGonchi <vpatrin95@mail.ru>
2019-10-25 23:23:28 +0300

    Module 3 task 4 was completed.

---
+++ Module3/Task_4/Task_4/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Task_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReplaceDigits();
+        }
+
+        static void ReplaceDigits()
+        {
+            int number;
+            int digit;
+
+            Console.Write("Введите число: ");
+            number = int.Parse(Console.ReadLine());
+            Console.Write("Число в обратном порядке: ");
+            if (number >= 0)
+            {
+                do
+                {
+                    digit = number % 10;
+                    Console.Write(digit);
+                    number /= 10;
+                } while (number > 0);
+            }
+            if (number < 0)
+            {
+                number *= -1;
+                Console.Write("-");
+                do
+                {
+                    digit = number % 10;
+                    Console.Write(digit);
+                    number /= 10;
+                } while (number > 0);
+            }
+        }
+    }
+}
