38a6f2d2b3e9b5cc04ad482c0b956e735329b0bc
AdskiGonchi <vpatrin95@mail.ru>
2019-10-31 23:38:08 +0300

    Module 4 task 6 was completed.

---
+++ Module4/Task_6/Task_6/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Task_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите длину массива: ");
+            int length = int.Parse(Console.ReadLine());
+            int[] Array = new int[length];
+            for (int i = 0; i < Array.Length; i++)
+            {
+                Console.Write("Введите значение {0} элемента массива: ", i + 1);
+                Array[i] = int.Parse(Console.ReadLine());
+            }
+
+            IncreaseArrayElementsByFive(Array);
+        }               
+
+        static void IncreaseArrayElementsByFive(int [] array)
+        {
+            for(int i = 0; i < array.Length; i++)
+            {
+                array[i] += 5;
+            }
+
+            Console.WriteLine("Массив после увеличения: ");
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (i != 0)
+                {
+                    Console.Write(", ");
+                }
+                Console.Write(array[i]);
+            }
+        }
+    }
+}
