f6241274ea45ca16c857a2b9d10dc05150ff3463
AdskiGonchi <vpatrin95@mail.ru>
2019-11-03 18:02:27 +0300

    Module 4 task 5 was completed.

---
+++ Module4/Task_5/Task_5/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Task_5
+{
+    enum Operation
+    {
+        Add=1,
+        Subtract,
+        Multiply,
+        Divide
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CalculateDays();
+           
+            MathOperation(10, 15, Operation.Add);
+           
+        }
+
+        static void MathOperation(double a, double b, Operation type)
+        {            
+            double result=0;
+            switch (type)
+            {
+                case Operation.Add:
+                    result = a + b;
+                    break;
+                case Operation.Subtract:
+                    result = a - b;
+                    break;
+                case Operation.Multiply:
+                    result = a * b;
+                    break;
+                case Operation.Divide:
+                    result = a / b;
+                    break;
+            }
+            Console.WriteLine("Результат вычисления равен: "+result);
+            
+        }
+
+        static void CalculateDays()
+        {
+            Console.Write("Введите номер месяца в году: ");
+            double x = double.Parse(Console.ReadLine());
+            double days;
+            days = 28 + (x + Math.Floor(x / 8)) % 2 + 2 % x + 2 * Math.Floor(1 / x);
+            int intdays = Convert.ToInt32(days);
+            Console.WriteLine("В данном месяце {0} дней", intdays);
+        }
+    }
+}
