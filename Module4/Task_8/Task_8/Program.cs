24b0853b283d077d13660b2c19b76d8bac48b469
AdskiGonchi <vpatrin95@mail.ru>
2019-11-03 18:03:42 +0300

    Module 4 task 8 was completed.

---
+++ Module4/Task_8/Task_8/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Task_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double accuracy = 0.0001;
+            double a = 0.5, b = 1;
+            double x = 0;
+            Console.WriteLine("Концы отрезков: {0} {1} ", a, b);
+            Console.WriteLine("Значение точности: " + accuracy);
+            HalfLength(ref a, ref b, ref x, accuracy);
+            
+            Console.WriteLine("Решение уравнения: " + x);
+        }
+
+        static double Function(double x)
+        {
+            return Math.Pow(x, 3) - Math.Sin(x);            
+        }
+
+        static void HalfLength(ref double a, ref double b, ref double x, double accuracy)
+        {           
+
+            x = (a + b) / 2;
+            if (Function(x) * Function(a) > 0)
+            {
+                a = x;
+            }
+            else
+            {
+                b = x;
+            }
+
+            if ((b - a) > accuracy)
+            {
+                HalfLength(ref a,ref  b, ref x, accuracy);
+            }
+            else return;            
+        }
+    }
+}
